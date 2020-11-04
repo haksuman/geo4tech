96f8512e4ae6d31b96068e7e46fdfda34c2b4d89
haksuman <51818946+haksuman@users.noreply.github.com>
2020-11-04 20:49:42 +0300

    static file update
    12321

--- geo4tech/settings.py
+++ geo4tech/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 ACCOUNT_ACTIVATION_DAYS = 7  # the time period that is given to activate account
 

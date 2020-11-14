a77a292f54db293e8e9826aa51c73a078ed28210
haksuman <51818946+haksuman@users.noreply.github.com>
2020-11-14 18:42:23 +0300

    Layer Number Update
    layer number update in spt_table and post request update

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,12 @@
 # urls.py
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("spt/", views.spt, name="spt")
+    path("spt/", views.spt, name="spt"),
+
+    # re_path yerine url koymayı dene normalde öyleydi
+    re_path(r'^spt_data_transfer$', views.spt_data_transfer, name='spt_data_transfer'),
 ]
